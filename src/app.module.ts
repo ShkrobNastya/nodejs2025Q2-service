3e22da8fa7be059eb016d0fb202916a4a1ee26ed
Nastya Shkrob <shkrobnastya@gmail.com>
2025-11-29 17:43:08 +0100

    add user validation and fix tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { TrackModule } from './modules/track/track.module';
+import { ArtistModule } from './modules/artist/artist.module';
+import { AlbumModule } from './modules/album/album.module';
 
 @Module({
-  imports: [UserModule, TrackModule],
+  imports: [UserModule, TrackModule, ArtistModule, AlbumModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,10 +6,16 @@ import {
   Put,
   Delete,
   Param,
+  HttpCode,
+  NotFoundException,
+  ParseUUIDPipe,
+  ForbiddenException,
 } from '@nestjs/common';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { Serialize } from 'src/common/interceptors/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
@@ -21,22 +27,42 @@ export class UserController {
   }
 
   @Get('/:id')
-  getUserById(@Param('id') id: string) {
-    return this.userService.getUserById(id);
+  getUserById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = this.userService.getUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   @Post()
+  @Serialize(UserDto)
   createUser(@Body() dto: CreateUserDto) {
     return this.userService.createUser(dto);
   }
 
   @Put('/:id')
-  updatePassword(@Param('id') id: string, @Body() dto: UpdatePasswordDto) {
-    return this.userService.updatePassword(id, dto);
+  @Serialize(UserDto)
+  updatePassword(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    const result = this.userService.updatePassword(id, dto);
+    if (!result) {
+      throw new NotFoundException('User not found');
+    }
+    if (result === 'Wrong password') {
+      throw new ForbiddenException('Old password is wrong');
+    }
+    return result;
   }
 
   @Delete('/:id')
-  deleteUser(@Param('id') id: string) {
-    return this.userService.deleteUser(id);
+  @HttpCode(204)
+  deleteUser(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = this.userService.deleteUser(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
   }
 }
