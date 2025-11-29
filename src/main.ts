3aa6a3ed39d3965e85bd61c421cfc0070e3545bc
Nastya Shkrob <shkrobnastya@gmail.com>
2025-11-29 23:34:16 +0100

    add swagger and fix yaml for users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import * as YAML from 'yamljs';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const port = process.env.PORT || 4000;
+
+  const swaggerDocument = YAML.load('./doc/api.yaml');
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
+  await app.listen(port);
 }
 bootstrap();
