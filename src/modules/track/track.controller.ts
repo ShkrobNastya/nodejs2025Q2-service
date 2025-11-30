890358e25c5a168846aa6f486a72b8a39227a80f
Nastya Shkrob <shkrobnastya@gmail.com>
2025-11-30 13:36:46 +0100

    fix yaml response messages

--- src/modules/track/track.controller.ts
+++ src/modules/track/track.controller.ts
@@ -44,7 +44,7 @@ export class TrackController {
   ) {
     const result = this.trackService.updateTrackInfo(id, dto);
     if (!result) {
-      throw new NotFoundException('Artist not found');
+      throw new NotFoundException('Track not found');
     }
     return result;
   }
